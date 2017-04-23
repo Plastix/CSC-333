16551227291a50fcb72a480c2a45c37e6a3c8ce0
Plastix <aidanpieper@gmail.com>
2017-04-23 19:34:18 -0400

    Project2: Add job submission script

---
+++ projects/Project2/run-bday.sh
@@ -0,0 +1,18 @@
+#PBS -l procs=1
+#PBS -l walltime=30:00
+#PBS -j oe
+#PBS -N runmpi
+
+if [ "$PBS_ENVIRONMENT" != "PBS_INTERACTIVE" ] ; then
+cd $PBS_O_WORKDIR
+fi
+
+typeset integer NPROCS
+NPROCS=`wc -l $PBS_NODEFILE | awk '{print $1}'`
+
+#location of mpi files
+#MPI_DIR=$HOME/some-mpi-directory
+
+##to use TCP
+mpirun --mca btl tcp,self --mca btl_tcp_if_include eth0 \
+       -hostfile $PBS_NODEFILE -np $NPROCS ./bday 77000 32
