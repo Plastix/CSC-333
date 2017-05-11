bd95bbb65e99b66e334a1bd3c85f3a2bb180acd1
Plastix <aidanpieper@gmail.com>
2017-05-11 15:55:23 -0400

    Add OpenMP matricies lab

---
+++ labs-and-demos/inclass-week7-17/matrices.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void randomizeMat(double *mat, int n, short *seed) {
+    int row, col;
+
+    for (row = 0; row < n; row++) {
+        for (col = 0; col < n; col++) {
+            mat[n * row + col] = erand48(seed);
+        }
+    }
+}
+
+void printMat(double *mat, int n) {
+    int row, col;
+    for (row = 0; row < n; row++) {
+        for (col = 0; col < n; col++) {
+            printf("%f ", mat[n * row + col]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+
+}
+
+void sum(double *mat1, double *mat2, int n, int threads, double *outMat) {
+    int row, col;
+    for (row = 0; row < n; row++) {
+
+#pragma omp parallel for num_threads(threads)
+        for (col = 0; col < n; col++) {
+            outMat[n * row + col] = mat1[n * row + col] + mat2[n * row + col];
+        }
+    }
+}
+
+void transpose(double *mat, int n, int threads, double *outMat) {
+    int row, col;
+    for (row = 0; row < n; row++) {
+
+#pragma omp parallel for num_threads(threads)
+        for (col = 0; col < n; col++) {
+            outMat[n * row + col] = mat[col * n + row];
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+
+    int N = 0;
+
+    // THIS WILL BE YOUR RANDOM SEED
+    // YES IT IS NON-RANDOM - Read the lab!
+    unsigned short xi[3] = {1, 9, 99};
+    int threadCount;
+
+    if (argc != 3) {
+        printf("usage: matrices <Matrix Size> <Num Threads>\n ");
+        exit(1);
+    } else {
+        N = atoi(argv[1]);
+        threadCount = atoi(argv[2]);
+
+        printf("Rows: %d, Cols: %d\n", N, N);
+    }
+
+    double *MAT = calloc(N * N, sizeof(double));
+    double *MAT2 = calloc(N * N, sizeof(double));
+    double *sumsMAT = calloc(N * N, sizeof(double));
+    double *transposeMat = calloc(N * N, sizeof(double));
+
+    randomizeMat(MAT, N, xi);
+    randomizeMat(MAT2, N, xi);
+    sum(MAT, MAT2, N, threadCount, sumsMAT);
+    transpose(MAT, N, threadCount, transposeMat);
+
+    printf("MAT1\n");
+    printMat(MAT, N);
+    printf("MAT2\n");
+    printMat(MAT2, N);
+    printf("ADD\n");
+    printMat(sumsMAT, N);
+    printf("TRANSPOSE\n");
+    printMat(transposeMat, N);
+
+    free(MAT);
+    free(MAT2);
+    free(sumsMAT);
+}
+
+
+
+
