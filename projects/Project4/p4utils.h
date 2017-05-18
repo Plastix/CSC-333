5cdaf9c3c490d00d1c836fc142153e20a0efc451
Plastix <aidanpieper@gmail.com>
2017-05-18 12:19:05 -0400

    Project4: Implement 1a (absolute edge detection)

--- projects/Project4/p4utils.h
+++ projects/Project4/p4utils.h
@@ -5,5 +5,6 @@ project 4 utilities
 */
 
 #include "PGM.h"
-void convolve(unsigned char inImg[MAXROWS][MAXCOLS],long rows, long cols,unsigned char outimg[MAXROWS][MAXCOLS], int kernel[3][3]);
-void cannyX(unsigned char inImg[MAXROWS][MAXCOLS],long rows, long cols,unsigned char outimg[MAXROWS][MAXCOLS]);
+
+void convolve(unsigned char inImg[MAXROWS][MAXCOLS], long rows, long cols, unsigned char outimg[MAXROWS][MAXCOLS],
+              int kernel[3][3]);
--- projects/Project4/project4.c
+++ projects/Project4/project4.c
@@ -9,56 +9,84 @@ based initially on demopgm.c  by Richard Zanibbi (May 1998) for
  */
 
 #include "PGM.h"
-#include "iroutines.h"
-#include "p4utils.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-void main (){
-    long rows,cols;                         /* dimensions of the pixmap */
+void edgeGradient(unsigned char inImg[MAXROWS][MAXCOLS], int rows, int cols, unsigned char outimg[MAXROWS][MAXCOLS]) {
+    int sobelXkernel[3][3] = {{1, 0, -1},
+                              {2, 0, -2},
+                              {1, 0, -1}};
+    int sobelYkernel[3][3] = {{1,  2,  1},
+                              {0,  0,  0},
+                              {-1, -2, -1}};
+
+    int row, col;
+    for (row = 0; row < rows; row++) {
+        for (col = 0; col < cols; col++) {
+
+            long xAcc = 0;
+            long yAcc = 0;
+            //instead of wrapping around a toroid we just repeat first/last coloum and row
+            int krow, kcol;
+
+            for (krow = 0; krow < 3; krow++) {
+                for (kcol = 0; kcol < 3; kcol++) {
+                    //get the kernel value
+                    int xval = sobelXkernel[krow][kcol];
+                    int yval = sobelYkernel[krow][kcol];
+
+                    int krowoffset = krow - 1;
+                    int kcoloffset = kcol - 1;
+                    int currow = row + krowoffset;
+                    int curcol = col + kcoloffset;
+
+                    currow = currow < 0 ? 0 : currow;
+                    curcol = curcol < 0 ? 0 : curcol;
+                    currow = currow > rows - 1 ? rows - 1 : currow;
+                    curcol = curcol > cols - 1 ? cols - 1 : curcol;
+                    xAcc += inImg[currow][curcol] * xval;
+                    yAcc += inImg[currow][curcol] * yval;
+                }
+            }
+
+            // Calculate
+            double val = sqrt(pow(xAcc, 2) + pow(yAcc, 2));
+            // Normalize value
+            outimg[row][col] = (unsigned char) fmin(fmax(val, 0), 255);
+        }
+    }
+}
+
+
+void main(int argc, char *argv[]) {
+    long rows, cols;                         /* dimensions of the pixmap */
     unsigned char image[MAXROWS][MAXCOLS];  /* 2D array to hold the image */
+    unsigned char out[MAXROWS][MAXCOLS];
 
-    unsigned char xsobel[MAXROWS][MAXCOLS];
-    unsigned char ysobel[MAXROWS][MAXCOLS];
-    char userInput[60];                     /* input from user */
-    int readOK,loopCondition,request;       /* two flags and user's request */
+    int readOK, loopCondition, request;     /* two flags and user's request */
     int writeOK;                           /* flag for succesful write */
-    loopCondition = 1;                     /* set loop condition to "true" */
-
-    printf ("input filename:\n"); /*  and manipulate it. */
-    scanf ("%s",userInput);
-    readOK = pgmRead (userInput,&rows,&cols,image);
-
-    // printf("there are %lu rows and %lu cols\n",rows,cols);
-     int row, col;
-    // for (row = 0; row < rows; row++){
-    //   for (col = 0; col < cols; col++) {
-    //       printf("%u ",image[row][col]);
-    //   }
-    //   printf("\n");
-    // }
 
 
-    int sobelXkernel[3][3] = {{1, 0, -1},{2, 0, -2},{1, 0,-1}};
-    int sobelYkernel[3][3] = {{-1, -2, -1},{0, 0, 0},{1, 2, 1}};
+    if (argc != 3) {
+        printf("Usage: %s <input image path> <output file name>\n", argv[0]);
+        exit(1);
+    }
 
-    //fill the xsobel matrix with the convolution
-    // of the X sobel Kernel
-    convolve(image,rows,cols,xsobel,sobelXkernel);
-    //fill the ysobel matrix with the convolution
-    // of the y sobel Kernel
-    convolve(image,rows,cols,ysobel,sobelYkernel);
+    printf("input filename:\n"); /*  and manipulate it. */
+    readOK = pgmRead(argv[1], &rows, &cols, image);
 
+    if (!readOK) {
+        printf("Error reading file: %s!\n", argv[1]);
+        exit(1);
+    }
 
-    // for (row = 0; row < rows; row++){
-    //   for (col = 0; col < cols; col++) {
-    //       printf("%u ",ysobel[row][col]);
-    //   }
-    //   printf("\n");
-    // }
+    edgeGradient(image, rows, cols, out);
 
-   //write the two files
-   pgmWrite("xsobel.pgm",rows,cols,xsobel,"test");
-   pgmWrite("ysobel.pgm",rows,cols,ysobel,"test");
+    writeOK = pgmWrite(argv[2], rows, cols, out, NULL);
+
+    if (writeOK) {
+        printf("Successfully processed image!\n");
+    }
 
 }
