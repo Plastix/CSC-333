352ec7745252a672625bf03f93be81f62b1f983b
Plastix <aidanpieper@gmail.com>
2017-05-20 13:19:50 -0400

    Project4: Implement serial Hough transform

--- projects/Project4/project4.c
+++ projects/Project4/project4.c
@@ -13,6 +13,12 @@ based initially on demopgm.c  by Richard Zanibbi (May 1998) for
 #include <stdlib.h>
 #include <math.h>
 
+#ifndef max
+#define max(a, b) ((a) > (b) ? (a) : (b))
+#endif
+
+#define degreesToRadians(angleDegrees) (angleDegrees * M_PI / 180.0)
+
 void edgeGradient(unsigned char inImg[MAXROWS][MAXCOLS], int rows, int cols,
                   unsigned char outimg[MAXROWS][MAXCOLS], int thread_num) {
     int sobelXkernel[3][3] = {{1, 0, -1},
@@ -57,6 +63,39 @@ void edgeGradient(unsigned char inImg[MAXROWS][MAXCOLS], int rows, int cols,
     }
 }
 
+int *lineDetect(unsigned char inImg[MAXROWS][MAXCOLS], int rows, int cols,
+                int thetaBins, int radiiBins, int thread_num,
+                unsigned char threshold) {
+
+
+    int *counts = calloc((size_t) (thetaBins * radiiBins), sizeof(int));
+
+    int maxR = max(rows, cols);
+    int radiiBinWidth = maxR / radiiBins;
+    int thetaBinWidth = 180 / thetaBins;
+
+    int row, col;
+    for (row = 0; row < rows; row++) {
+        for (col = 0; col < cols; col++) {
+            unsigned char pixel = inImg[row][col];
+
+            if (pixel >= threshold) {
+
+                int theta;
+                for (theta = 0; theta < 180; theta += thetaBinWidth) {
+                    double rad = degreesToRadians(theta);
+                    double r = col * cos(rad) + row * sin(rad);
+                    int radiiBin = (int) floor(fabs(r) / radiiBinWidth);
+                    int thetaBin = theta / thetaBinWidth;
+
+                    counts[thetaBins * radiiBin + thetaBin]++;
+                }
+            }
+        }
+    }
+
+    return counts;
+}
 
 void main(int argc, char *argv[]) {
     int thread_num;
@@ -83,7 +122,20 @@ void main(int argc, char *argv[]) {
         exit(1);
     }
 
+    int thetaBins = 20;
+    int radiiBins = 20;
     edgeGradient(image, rows, cols, out, thread_num);
+    int *counts = lineDetect(out, rows, cols, thetaBins, radiiBins, thread_num, 200);
+
+    for (int i = 0; i < radiiBins; i++) {
+        for (int j = 0; j < thetaBins; j++) {
+            printf("%i ", counts[i * thetaBins + j]);
+        }
+        printf("\n");
+    }
+
+
+    free(counts);
 
     writeOK = pgmWrite(argv[2], rows, cols, out, NULL);
 
