b4d8a6a079256bd51e588f4b0d5997b039c6424f
Plastix <aidanpieper@gmail.com>
2017-05-29 13:21:22 -0400

    Mild code refactor Real memory management of structs

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
 #include <time.h>
 #include <printf.h>
+#include <stdlib.h>
+#include <math.h>
 #include <string.h>
 #include "ppm.h"
 #include "ppm_utils.h"
@@ -13,34 +15,52 @@ typedef struct {
     int length;
 } PointList;
 
-void printPoint(Point point) {
-    printf("(%i, %i)\n", point.x, point.y);
+
+// Constructor for creating a new PointList
+// Allocates space on the heap so use freePointList(list) after
+PointList *pointList(int n) {
+    PointList *result = calloc(1, sizeof(PointList));
+    result->length = n;
+    result->points = calloc((size_t) n, sizeof(Point));
+    return result;
 }
 
-void printPointList(PointList pointList) {
-    printf("--- Set ---\n");
-    int i;
-    for (i = 0; i < pointList.length; i++) {
-        printPoint(pointList.points[i]);
-    }
-}
+// Destructor for PointLists
+void freePointList(PointList *pointList) {
+    free(pointList->points);
+    free(pointList);
+};
+
+// Useful debugging commands
+//void printPoint(Point point) {
+//    printf("(%i, %i)\n", point.x, point.y);
+//}
+//
+//void printPointList(PointList pointList, char *label) {
+//    printf("--- %s ---\n", label);
+//    int i;
+//    for (i = 0; i < pointList.length; i++) {
+//        printPoint(pointList.points[i]);
+//    }
+//}
 
 int ccw(Point a, Point b, Point c) {
     return (b.x - a.x) * (c.y - a.y) - (b.y - a.y) * (c.x - a.x);
 }
 
-PointList convexHull(PointList pointList) {
-    int n = pointList.length;
+PointList *convexHull(PointList *list) {
+    printf("Calculating convex hull...\n");
+    int n = list->length;
 
-    Point hull[n];
+    Point *hull = calloc((size_t) (2 * n), sizeof(Point));
     int index = 0;
 
     int x;
     for (x = 0; x < n; x++) {
-        Point a = pointList.points[x];
+        Point a = list->points[x];
         int y;
         for (y = 0; y < n; y++) {
-            Point b = pointList.points[y];
+            Point b = list->points[y];
 
             if (a.x == b.x && a.y == b.y) {
                 continue;
@@ -49,7 +69,7 @@ PointList convexHull(PointList pointList) {
             int isConvex = 1;
             int k;
             for (k = 0; k < n; k++) {
-                Point c = pointList.points[k];
+                Point c = list->points[k];
 
                 if ((a.x == c.x && a.y == c.y) || (b.x == c.x && b.y == c.y)) {
                     continue;
@@ -68,17 +88,57 @@ PointList convexHull(PointList pointList) {
         }
     }
 
-    PointList result;
-    result.length = index;
-    result.points = calloc((size_t) index + 1, sizeof(Point));
-    memcpy(result.points, hull, (index + 1) * sizeof(Point));
+    PointList *result = pointList(index);
+    memcpy(result->points, hull, (index) * sizeof(Point));
+    free(hull);
 
     return result;
 }
 
-int main(int argc, char *argv[]) {
+void drawHull(ppm_image *image, PointList *hull) {
+    printf("Drawing hull...\n");
+    int i;
+    for (i = 0; i < hull->length - 1; i += 2) {
+        Point one = hull->points[i];
+        Point two = hull->points[i + 1];
+        draw_circle(image, one.x, one.y, 10, 255, 0, 255);
+        draw_circle(image, two.x, two.y, 10, 255, 0, 255);
+        line(image, one.x, one.y, two.x, two.y, 0, 0, 255);
+    }
 
-    srand((unsigned int) time(NULL));
+}
+
+PointList *generatePoints(int numPoints, int w, int h) {
+    printf("Generating points...\n");
+    PointList *points = pointList(numPoints);
+    int i = 0;
+    while (i < numPoints) {
+        int x = (rand() % w);
+        int y = (rand() % h);
+
+        if (hypot(x - (w / 2.0), y - (h / 2.0)) <= 0.4 * w) {
+            points->points[i].x = x;
+            points->points[i].y = y;
+            i++;
+        }
+
+    }
+
+    return points;
+}
+
+void drawPoints(ppm_image *image, PointList *points) {
+    int i;
+    for (i = 0; i < points->length; i++) {
+        Point point = points->points[i];
+        draw_circle(image, point.x, point.y, 2, 0, 0, 0);
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+    unsigned int seed = (unsigned int) time(NULL);
+    srand(seed);
 
     if (argc != 4) {
         printf("Usage: %s <width> <height> <num points>\n", argv[0]);
@@ -88,43 +148,32 @@ int main(int argc, char *argv[]) {
     int w = atoi(argv[1]);
     int h = atoi(argv[2]);
 
+    // Setup image
     ppm_image *image = make_image(w, h);
     set_background_color(image, 255, 255, 255);
 
     int numPoints = atoi(argv[3]);
-    PointList points;
-    points.length = numPoints;
-    points.points = calloc((size_t) points.length, sizeof(Point));
-
-    printf("Generating points...\n");
-    int i = 0;
-    while (i < numPoints) {
-        int x = (rand() % w);
-        int y = (rand() % h);
-
-        if (hypot(x - (w / 2.0), y - (h / 2.0)) <= 0.4 * w) {
-            points.points[i].x = x;
-            points.points[i].y = y;
-            draw_circle(image, x, y, 1, 0, 0, 0);
-            i++;
-        }
-
+    if (numPoints < 2) {
+        printf("Number of points must be at least 2!");
+        exit(1);
     }
 
-    printf("Calculating convex hull...\n");
-    PointList hull = convexHull(points);
+    printf("Random Seed: %i\n", seed);
 
-    printf("Drawing hull...\n");
-    for (i = 0; i < hull.length - 1; i += 2) {
-        Point one = hull.points[i];
-        Point two = hull.points[i + 1];
-        draw_circle(image, one.x, one.y, 10, 255, 0, 255);
-        draw_circle(image, two.x, two.y, 10, 255, 0, 255);
-        line(image, one.x, one.y, two.x, two.y, 0, 0, 255);
-    }
+    // Generate random points and draw points on image
+    PointList *points = generatePoints(numPoints, w, h);
+    drawPoints(image, points);
 
-    free(points.points);
-    write_image(image, "test.ppm");
+    // Calculate convex hull
+    PointList *hull = convexHull(points);
+
+    // Draw convex hull on image
+    drawHull(image, hull);
+
+    // Free resources and save image
+    freePointList(points);
+    freePointList(hull);
+    write_image(image, "hull.ppm");
     free_image(image);
 
     return 0;
